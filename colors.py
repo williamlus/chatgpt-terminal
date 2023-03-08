3a2dde75c2e8e35cf15ffc4d0c3e99320ccb3e69
williamlus <luwilam29@gmail.com>
2023-03-08 22:13:21 +0800

    add colors

---
+++ colors.py
@@ -0,0 +1,53 @@
+class colors:
+ 
+ 
+    '''Colors class:reset all colors with colors.reset; two
+    sub classes fg for foreground
+    and bg for background; use as colors.subclass.colorname.
+    i.e. colors.fg.red or colors.bg.greenalso, the generic bold, disable,
+    underline, reverse, strike through,
+    and invisible work with the main class i.e. colors.bold'''
+    
+    def get_color(color: str, fg=True):
+        if fg:
+            try: plain_color_str=getattr(colors.fg,color)
+            except: plain_color_str=getattr(colors,color)
+        else:
+            try: plain_color_str=getattr(colors.bg,color)
+            except: plain_color_str=getattr(colors,color)
+        return plain_color_str
+    
+    reset = '\033[0m'
+    bold = '\033[01m'
+    disable = '\033[02m'
+    underline = '\033[04m'
+    reverse = '\033[07m'
+    strikethrough = '\033[09m'
+    invisible = '\033[08m'
+    
+    class fg:
+        black = '\033[30m'
+        red = '\033[31m'
+        green = '\033[32m'
+        orange = '\033[33m'
+        blue = '\033[34m'
+        purple = '\033[35m'
+        cyan = '\033[36m'
+        lightgrey = '\033[37m'
+        darkgrey = '\033[90m'
+        lightred = '\033[91m'
+        lightgreen = '\033[92m'
+        yellow = '\033[93m'
+        lightblue = '\033[94m'
+        pink = '\033[95m'
+        lightcyan = '\033[96m'
+    
+    class bg:
+        black = '\033[40m'
+        red = '\033[41m'
+        green = '\033[42m'
+        orange = '\033[43m'
+        blue = '\033[44m'
+        purple = '\033[45m'
+        cyan = '\033[46m'
+        lightgrey = '\033[47m'
