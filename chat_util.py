486c7d4e64e5dcb75bb5e817a881432bb45bd25d
williamlus <luwilam29@gmail.com>
2023-03-20 12:27:22 +0800

    change back

--- chat_util.py
+++ chat_util.py
@@ -162,7 +162,7 @@ def ask_question(ques:list):
             elif "content" in delta:
                 ans+=delta["content"]
                 print(delta["content"], end="", flush=True)
-            else: break
+            else: continue
         print()
         del response
     except Exception as e:
