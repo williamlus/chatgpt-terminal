48efc7090f6cbb67269df12176c7ace252728be2
williamlus <luwilam29@gmail.com>
2023-03-20 13:10:58 +0800

    delete timeout

--- chat_util.py
+++ chat_util.py
@@ -154,7 +154,7 @@ def ask_question(ques:list):
     ans=""
     response=None
     try:
-        response=openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=ques,stream=True, timeout=10)
+        response=openai.ChatCompletion.create(model="gpt-3.5-turbo",messages=ques,stream=True)
         for r in response:
             delta=r.choices[0].delta
             if "role" in delta:
